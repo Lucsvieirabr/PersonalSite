c6d1ebb1b32aff74fd67bb1f9027f7bb53d81f4a
FIRST_NAME LAST_NAME <62450294+Lucsvieirabr@users.noreply.github.com>
2022-05-26 07:24:06 -0300

    Adjust Bg img to an Div

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function current_API(){
 function request_API_And_ChangeHTML(){
     var request_API = new Request(`https://awesome-api.vercel.app/api/${current_API()}`)
     fetch(request_API).then(response => response.json()).then(json =>{
-        document.getElementById('html').style.backgroundImage= "url(" + json.img +')';
-        document.getElementById('html').style.backgroundPosition= 'center';
+        document.getElementById('imageBg').style.backgroundImage= "url(" + json.img +')';
+        document.getElementById('imageBg').style.backgroundPosition= 'center';
         document.getElementById('title').innerHTML =  json.title || json.name;
     });
 }
@@ -25,14 +25,12 @@ function changeBackgroundType(){
 
     var backtype = document.getElementById("backtype").value;
     if(backtype === "repeat"){
-        document.getElementById('html').style.backgroundRepeat='round';
-        document.getElementById('html').style.backgroundSize='20%';
+        document.getElementById('imageBg').style.backgroundRepeat='round';
 
     }
 
     if(backtype === "cover"){
-        document.getElementById('html').style.backgroundSize='20%';
-        document.getElementById('html').style.backgroundRepeat='no-repeat';
+        document.getElementById('imageBg').style.backgroundRepeat='no-repeat';
 
     }
 }
