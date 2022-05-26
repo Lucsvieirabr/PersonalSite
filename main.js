804e305228df353246288d829cba774c916db3cb
FIRST_NAME LAST_NAME <62450294+Lucsvieirabr@users.noreply.github.com>
2022-05-26 15:04:06 -0300

    Bg Gradient to dominant color

--- main.js
+++ main.js
@@ -6,19 +6,28 @@ function request_API(){
     return new Request(`https://awesome-api.vercel.app/api/${current_API()}`)
 }
 
-async function teste(){
+async function ediBg_toGradientColor(){
     const colorThief = new ColorThief();
     const img = new Image();
-
-    img.addEventListener('load', function() {
-        console.log(colorThief.getPalette(img));
-    });
-
-    img.crossOrigin = 'Anonymous';
     var json = await get_API_JSON();
+    img.crossOrigin = 'Anonymous';
     img.src = json.img;
+
+    img.addEventListener('load',function() {
+        var colorpalette = colorThief.getPalette(img);
+        var dominnatcolor = colorThief.getColor(img);
+        dominantcolor = rgbToHex(dominnatcolor[0], dominnatcolor[1], dominnatcolor[2]);
+        var colorArray = [];
+        colorpalette.forEach(function(rgb){
+            var colorHex = rgbToHex(rgb[0], rgb[1], rgb[2]);
+            colorArray.push(colorHex);
+        });
+        document.getElementById('html').style.background= `linear-gradient(to bottom right,${dominantcolor}, ${colorArray[1]})`;
+    });
 }
-teste();
+function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
 async function get_API_JSON(){
     return await fetch(request_API()).then(response => response.json()).then(json =>{
         return json;
@@ -30,6 +39,7 @@ async function changeHTML_to_currentAPI(){
     document.getElementById('imageBg').style.backgroundImage= "url(" + json.img +')';
     document.getElementById('imageBg').style.backgroundPosition= 'center';
     document.getElementById('title').innerHTML =  json.title || json.name;
+    ediBg_toGradientColor();
 }
 
 function OpenMenu(){
@@ -51,6 +61,7 @@ function changeBackgroundType(){
     }
     if(backtype === "cover"){
         document.getElementById('imageBg').style.backgroundRepeat='no-repeat';
+        ediBg_toGradientColor();
     }
 }
 changeHTML_to_currentAPI();
