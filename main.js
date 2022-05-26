d736e9690e541b5485af07980cc2bfb26abc8148
FIRST_NAME LAST_NAME <62450294+Lucsvieirabr@users.noreply.github.com>
2022-05-26 10:10:09 -0300

    Testing API colorThief

--- main.js
+++ main.js
@@ -6,6 +6,19 @@ function request_API(){
     return new Request(`https://awesome-api.vercel.app/api/${current_API()}`)
 }
 
+async function teste(){
+    const colorThief = new ColorThief();
+    const img = new Image();
+
+    img.addEventListener('load', function() {
+        console.log(colorThief.getPalette(img));
+    });
+
+    img.crossOrigin = 'Anonymous';
+    var json = await get_API_JSON();
+    img.src = json.img;
+}
+teste();
 async function get_API_JSON(){
     return await fetch(request_API()).then(response => response.json()).then(json =>{
         return json;
